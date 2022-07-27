c516a5333cfff92eddc5f2909e579d818ebc2479
Micah Van Deusen <micahjvandeusen@gmail.com>
2022-07-26 19:24:04 -0500

    Better handling of LDAP
    It is not possible (AFAIK) to retrieve the gMSA password over non-LDAPS. Added better error messages and support for ACL over LDAP. Closes #8

--- gMSADumper.py
+++ gMSADumper.py
@@ -77,17 +77,29 @@ def main():
         conn = Connection(server, user='{}\\{}'.format(args.domain, args.username), password=args.password, authentication=NTLM, auto_bind=True)
     else:
         conn = Connection(server, authentication=SASL, sasl_mechanism=KERBEROS, auto_bind=True)
-    conn.start_tls()
-    success = conn.search(base_creator(args.domain), '(&(ObjectClass=msDS-GroupManagedServiceAccount))', search_scope=SUBTREE, attributes=['sAMAccountName','msDS-ManagedPassword','msDS-GroupMSAMembership'])
+
+    ldaps = False
+    try:
+        conn.start_tls()
+        ldaps = True
+    except:
+        print('Unable to start a TLS connection. Is LDAPS enabled? Only ACLs will be listed and not ms-DS-ManagedPassword.\n')
+
+    if ldaps:
+        success = conn.search(base_creator(args.domain), '(&(ObjectClass=msDS-GroupManagedServiceAccount))', search_scope=SUBTREE, attributes=['sAMAccountName','msDS-ManagedPassword','msDS-GroupMSAMembership'])
+    else:
+        success = conn.search(base_creator(args.domain), '(&(ObjectClass=msDS-GroupManagedServiceAccount))', search_scope=SUBTREE, attributes=['sAMAccountName','msDS-GroupMSAMembership'])
     
     if success:
+        if len(conn.entries) == 0:
+            print('No gMSAs returned.')
         for entry in conn.entries:
                 sam = entry['sAMAccountName'].value
                 print('Users or groups who can read password for '+sam+':')
                 for dacl in SR_SECURITY_DESCRIPTOR(data=entry['msDS-GroupMSAMembership'].raw_values[0])['Dacl']['Data']:
                     conn.search(base_creator(args.domain), '(&(objectSID='+dacl['Ace']['Sid'].formatCanonical()+'))', attributes=['sAMAccountName'])
                     print(' > ' + conn.entries[0]['sAMAccountName'].value)
-                if entry['msDS-ManagedPassword']:
+                if 'msDS-ManagedPassword' in entry and entry['msDS-ManagedPassword']:
                     data = entry['msDS-ManagedPassword'].raw_values[0]
                     blob = MSDS_MANAGEDPASSWORD_BLOB()
                     blob.fromString(data)
@@ -96,6 +108,9 @@ def main():
                     passwd = binascii.hexlify(hash.digest()).decode("utf-8")
                     userpass = sam + ':::' + passwd
                     print(userpass)
+    else:
+        print('LDAP query failed.')
+        print(success)
 
 if __name__ == "__main__":
     main()
